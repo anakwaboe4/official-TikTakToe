d75b250ffc539b0c994c16d64905566bcb14bb42
Ewout Hebbrecht <ewout.hebbrecht@student@student.odisee.be>
2023-02-08 20:15:44 +0100

    changed controller to code library

---
+++ TikTakToe/TikTakToe.Controller/Controller.cs
@@ -0,0 +1,23 @@
+﻿using TikTakToe.Core.Boards;
+using TikTakToe.Core.Participants;
+
+namespace TikTakToe.Console
+{
+    public class Controller
+    {
+        public List<Participant> Participants { get; set; }
+        public Board Board { get; set; }
+
+        public Controller()
+        {
+            Participants = new List<Participant>() { new Player(), new Player() };
+            Board = new Board();
+        }
+
+        public Controller(List<Participant> participants, Board board)
+        {
+            Participants = participants;
+            Board = board;
+        }
+    }
+}
