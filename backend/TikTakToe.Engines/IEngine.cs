755aabad60f7cdfa1597ae16a29dce6ebdc81d51
Laurent Aerens <Laurent.Aerens@codit.eu>
2023-11-04 16:19:01 +0100

    added enum to interface

--- backend/TikTakToe.Engines/IEngine.cs
+++ backend/TikTakToe.Engines/IEngine.cs
@@ -1,7 +1,9 @@
-﻿namespace TikTakToe.Engines
+﻿using TikTakToe.Core.Enums;
+namespace TikTakToe.Engines
 {
     public interface IEngine
     {
+        public Core.Enums.Engines Engine {  get; }
         public int MakeMove(int move);
         public int SetPos(int position);
         public double Bench(int depth);
