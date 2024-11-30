90c56ad5a6519f16608e8965f35b9fd6a11c3801
Ewout Hebbrecht <hebbrechte@delawareconsulting.com>
2024-11-30 14:35:21 +0100

    added initial migration

--- backend/TikTakToe.Repositories/Models/GameItem.cs
+++ backend/TikTakToe.Repositories/Models/GameItem.cs
@@ -5,17 +5,29 @@ using UUIDNext;
 
 namespace TikTakToe.Repositories.Models
 {
-    public class GameItem(int gridSizeX, int gridSizeY, List<Engines> engines, List<Squares> boardSquares)
+    public class GameItem
     {
+        public GameItem() { }
+
+        public GameItem(int gridSizeX, int gridSizeY, List<Engines> engines, List<Squares> boardSquares)
+        {
+            Id = Uuid.NewDatabaseFriendly(Database.SQLite);
+            Move = 0;
+            GridSizeX = gridSizeX;
+            GridSizeY = gridSizeY;
+            Engines = engines;
+            BoardSquares = boardSquares;
+        }
+
         [Key]
         public Guid Id { get; set; } = Uuid.NewDatabaseFriendly(Database.SQLite);
         public int Move { get; set; } = 0;
-        public int GridSizeX { get; set; } = gridSizeX;
-        public int GridSizeY { get; set; } = gridSizeY;
+        public int GridSizeX { get; set; }
+        public int GridSizeY { get; set; }
         [NotMapped]
-        public List<Engines> Engines { get; set; } = engines;
+        public List<Engines> Engines { get; set; } = [];
         [NotMapped]
-        public List<Squares> BoardSquares { get; set; } = boardSquares;
+        public List<Squares> BoardSquares { get; set; } = [];
         public ICollection<MoveItem> Moves { get; set; } = [];
     }
 }
